2a8f7f55b2b56395192c1fc2d5709a3268087189
Mac <mazharulislame.cse@gmail.com>
2020-12-13 01:24:13 +0600

    13 Scoped Access with InheritedWidget and Provider package

--- lib/app/email_sign_in_form.dart
+++ lib/app/email_sign_in_form.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
+import 'package:provider/provider.dart';
 import 'package:time_tracker/app/sign_in/validators.dart';
 import 'package:time_tracker/common_widget/for_submit_button.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/common_widget/platform_alert_dialog.dart';
-import 'package:time_tracker/services/auth_provider.dart';
+import 'package:time_tracker/services/auth.dart';
 
 enum EmailSignInFormType { signIn, register }
 
@@ -35,7 +36,7 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       _isLoading = true;
     });
     try {
-      final auth = AuthProvider.of(context);
+      final auth = Provider.of<AuthBase>(context, listen: false);
 
       if (_formType == EmailSignInFormType.signIn) {
         await auth.signInWithEmailAndPassword(_email, _password);
